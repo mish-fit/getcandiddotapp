24132cc5031d36250d88684c9b6c335db7c0b8f2
kandurisv <30374472+kandurisv@users.noreply.github.com>
2022-02-07 22:01:17 +0530

    changes made to dashboard

---
+++ src/components/dashboard/Sidebar/UserCard.js
@@ -0,0 +1,23 @@
+/** @jsxRuntime classic */
+/** @jsx jsx */
+import { jsx, Container, Flex, Image, Text } from "theme-ui";
+import firebase from "firebase";
+import { auth, googleAuthProvider } from "../../../lib/firebase";
+import { Button } from "@chakra-ui/react";
+import { useRouter } from "next/router";
+
+// Add a custom Link
+export function UserCard() {
+  const router = useRouter();
+
+  const addLinks = () => {
+    console.log("add links");
+  };
+
+  return (
+    <Container>
+      <Text>User Image</Text>
+      <Text>UserName</Text>
+    </Container>
+  );
+}
