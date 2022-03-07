8e3918caa878d701dbcd6b0bcb6fe8639df8a267
kandurisv <30374472+kandurisv@users.noreply.github.com>
2022-03-07 23:47:39 +0530

    Update api.js

--- src/lib/api.js
+++ src/lib/api.js
@@ -1,6 +1,6 @@
 export const authapi = process.env.NEXT_PUBLIC_AUTH_API;
 export const nonauthapi = process.env.NEXT_PUBLIC_NON_AUTH_API;
-export const s3url = process.env.NEXT_PUBLIC_S3_URL;
+export const s3url = process.env.NEXT_PUBLIC_S3URL;
 
 // export const authapi =
 //   "https://sgsz9rwhde.execute-api.ap-south-1.amazonaws.com/dev/";
